a119933b47c3d3046cb933bb48d8dfb6d5a43b3a
ujwalramesh <ujwalramesh@gmail.com>
2014-07-30 10:58:39 -0400

    started nlp slover

---
+++ code/Design/DesignSolveWnnlp.cxx
@@ -0,0 +1,21 @@
+#include <Design.h>
+
+#include "wnlib.h"
+#include "wnasrt.h"
+#include "wnsll.h"
+#include "wnnlp.h"
+
+void
+getObjFuncInWnnlpFormat()
+{
+
+}
+
+
+
+void
+Design::DesignSolveForAllCellsWnnlp(void)
+{
+
+
+}
